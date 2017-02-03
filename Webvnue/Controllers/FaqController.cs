6eff1e101c07a3cafa02bf531d5b0c3e5f4df7ec
Ken Hoac <khoac@calstatela.edu>
2017-02-02 23:47:55 -0800

    fool

---
+++ Webvnue/Controllers/FaqController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webvnue.Controllers
+{
+    public class faqController : Controller
+    {
+        // GET: faq
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
