8f104cdcbb491624b2906578b734e4e28c608e5b
Ken Hoac <khoac@calstatela.edu>
2017-05-23 23:04:52 -0700

    added comments to posts in db

---
+++ Webvnue/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Webvnue.Models
+{
+    public class Comment
+    {
+        public string Id { get; set; }
+        public string OriginalUserId { get; set; }
+        public string Message { get; set; }
+        public DateTime TimeStamp { get; set; }
+        [NotMapped]
+        public Models.MyIdentityUser OriginalUser { get; set; }
+    }
+}
