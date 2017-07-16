8c462becad7e31b1522215420f565b28c3d83f44
Josh <Jtpeterson2011@gmail.com>
2017-07-16 03:25:42 -0400

    push

--- Webvnue/Scripts/webvnue-scripts.js
+++ Webvnue/Scripts/webvnue-scripts.js
@@ -371,6 +371,41 @@
     document.querySelector('button').addEventListener('click', saveFile, false);
 
 
+
+
+
+
+
+    var imageLoader = document.getElementById('uploadMainImage');
+    imageLoader.addEventListener('change', handleImage, false);
+    var canvas = document.getElementById('imageCanvas');
+    var ctx = canvas.getContext('2d');
+
+
+    function handleImage(e){
+    var reader = new FileReader();
+    reader.onload = function(event){
+        var img = new Image();
+        img.onload = function(){
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img,0,0);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);     
+}
+
+
+
+
+
+
+
+
+
+
+
     /*
     window.onload = function () {
         var objDivs = document.getElementsByClassName('myBox')
